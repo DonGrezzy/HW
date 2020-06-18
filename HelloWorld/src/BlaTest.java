222640ecb95f7019911852f06590fd0abdb2ef5c
qba_moskwa <qba_moskwa@192.168.202.136>
2020-06-18 14:42:14 +0200

    ?

--- HelloWorld/src/BlaTest.java
+++ HelloWorld/src/BlaTest.java
@@ -4,6 +4,8 @@ public class BlaTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Testtest");
+		System.out.println("Testtest");
+		System.out.println("Testtest");
 	}
 
 }
