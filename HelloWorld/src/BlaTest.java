3dc8bf04c196d8f24397fd161646ade8ef5982c6
julee09 <julee09@t-online.de>
2020-06-18 14:47:38 +0200

    mmm,

--- HelloWorld/src/BlaTest.java
+++ HelloWorld/src/BlaTest.java
@@ -3,7 +3,9 @@ public class BlaTest {
 
 	public static void main(String[] args) {
 		System.out.println("Kann ich pushen1?");
-
+		//NExtextext
+		//sdfghjk
+		System.out.println("Testtest");
 	}
 
 }
