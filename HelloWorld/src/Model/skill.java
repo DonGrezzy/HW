5e100f5d45a89ed896bfe18051102e684107f881
DESKTOP-GMCU2QG\User <izamrik@gmail.com>
2020-06-19 13:53:42 +0200

    Skill Name und Damgevalue

--- HelloWorld/src/Model/skill.java
+++ HelloWorld/src/Model/skill.java
@@ -1,5 +1,25 @@
 package Model;
 
 public class skill {
+	String name;
+	int dmagevalue;
+	
+	public skill(String name, int dmagevalue) {
+		this.name = name;
+		this.dmagevalue = dmagevalue;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDmagevalue() {
+		return dmagevalue;
+	}
+	public void setDmagevalue(int dmagevalue) {
+		this.dmagevalue = dmagevalue;
+	}
+	
 	
 }
